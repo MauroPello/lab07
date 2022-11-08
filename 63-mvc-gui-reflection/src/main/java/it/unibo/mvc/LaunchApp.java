ad1e9082723fb8322dc34962e95f4da6f8c1828a
Mauro Pellonara <mauro.pellonara@studio.unibo.it>
2022-11-08 17:25:36 +0100

    Remove unused dependecies

--- 63-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
+++ 63-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
@@ -7,8 +7,6 @@ import it.unibo.mvc.api.DrawNumberController;
 import it.unibo.mvc.api.DrawNumberView;
 import it.unibo.mvc.controller.DrawNumberControllerImpl;
 import it.unibo.mvc.model.DrawNumberImpl;
-import it.unibo.mvc.view.DrawNumberSwingView;
-import it.unibo.mvc.view.DrawNumberStdoutView;
 
 /**
  * Application entry-point.
