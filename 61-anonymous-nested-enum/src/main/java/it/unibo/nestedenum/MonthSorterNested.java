65fe33a76518b5b86f6871ea88c822cf4ae87cbe
Mauro Pellonara <mauro.pellonara@studio.unibo.it>
2022-11-08 15:59:33 +0100

    Modify code after teacher corrections

--- 61-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 61-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -8,18 +8,18 @@ import java.util.Objects;
  */
 public final class MonthSorterNested implements MonthSorter {
     enum Month {
-        JANUARY     (31),
-        FEBRUARY    (28),
-        MARCH       (31),
-        APRIL       (30),
-        MAY         (31),
-        JUNE        (30),
-        JULY        (31),
-        AUGUST      (31),
-        SEPTEMBER   (30),
-        OCTOBER     (31),
-        NOVEMBER    (30),
-        DECEMBER    (31);
+        JANUARY (31),
+        FEBRUARY (28),
+        MARCH (31),
+        APRIL (30),
+        MAY (31),
+        JUNE (30),
+        JULY (31),
+        AUGUST (31),
+        SEPTEMBER (30),
+        OCTOBER (31),
+        NOVEMBER (30),
+        DECEMBER (31);
 
         private final int days;
 
@@ -29,7 +29,10 @@ public final class MonthSorterNested implements MonthSorter {
 
         public static Month fromString(final String month) {
             // argument can't be null
-            Objects.requireNonNull(month);
+            if (month == null) {
+                throw new IllegalArgumentException("The passed string is null");
+            }
+
             try {
                 // if we get a perfect match there is no need to search further
                 return Month.valueOf(month);
@@ -38,7 +41,7 @@ public final class MonthSorterNested implements MonthSorter {
                 // searching for partial matches
                 for (final Month elem : Month.values()) {
                     // ignoring case but the matching string has to be at the start of the month
-                    if (elem.toString().toLowerCase().startsWith(month.toLowerCase())){
+                    if (elem.toString().toLowerCase().indexOf(month.toLowerCase()) == 0){
                         // if we already got a match then it's ambiguous
                         if (match != null) {
                             throw new IllegalArgumentException("The string passed as argument is ambiguous!");
