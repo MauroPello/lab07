bae00d6578a961e4839a9cecec5c463081d67824
Mauro Pellonara <mauro.pellonara@studio.unibo.it>
2022-11-08 16:52:51 +0100

    Add multiple view functionality

--- 63-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
+++ 63-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
@@ -4,6 +4,7 @@ import it.unibo.mvc.api.DrawNumberController;
 import it.unibo.mvc.controller.DrawNumberControllerImpl;
 import it.unibo.mvc.model.DrawNumberImpl;
 import it.unibo.mvc.view.DrawNumberSwingView;
+import it.unibo.mvc.view.DrawNumberStdoutView;
 
 /**
  * Application entry-point.
@@ -27,5 +28,9 @@ public final class LaunchApp {
         final var model = new DrawNumberImpl();
         final DrawNumberController app = new DrawNumberControllerImpl(model);
         app.addView(new DrawNumberSwingView());
+        app.addView(new DrawNumberSwingView());
+        app.addView(new DrawNumberStdoutView());
+        // for each class that implements DrawNumberView
+            // add it 3 times
     }
 }
---
+++ 63-mvc-gui-reflection/src/main/java/it/unibo/mvc/view/DrawNumberStdoutView.java
@@ -0,0 +1,29 @@
+package it.unibo.mvc.view;
+
+import it.unibo.mvc.api.DrawNumberController;
+import it.unibo.mvc.api.DrawNumberView;
+import it.unibo.mvc.api.DrawResult;
+
+public class DrawNumberStdoutView implements DrawNumberView {
+    private static final String NEW_GAME = ": a new game starts!";
+
+    public DrawNumberStdoutView() { }
+
+    public void start() { 
+        System.out.println("Game started!");
+    }
+
+    public void setController(DrawNumberController observer) { }
+
+    public void result(final DrawResult res) {
+        switch (res) {
+            case YOURS_HIGH, YOURS_LOW -> {
+                System.out.println(res.getDescription());
+                return;
+            }
+            case YOU_WON -> System.out.println(res.getDescription() + NEW_GAME);
+            case YOU_LOST -> System.out.println(res.getDescription() + NEW_GAME);
+            default -> throw new IllegalStateException("Unknown game state");
+        }
+    }
+}
